eae943c549e5caf4f41761b8ceb367048c7483a6
cuongnc0211 <cuongnguyenfu@gmail.com>
2020-02-11 22:49:26 +0700

    add fruit

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -4,6 +4,29 @@ class Body extends React.Component {
     this.state = {
       fruits: []
     }
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.addNewFruit = this.addNewFruit.bind(this);
+  }
+
+  handleFormSubmit(name, description) {
+    console.log(name, description);
+    let body = JSON.stringify({fruit: {name: name, description:   description} })
+    fetch('http://localhost:3000/api/v1/fruits', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: body,
+    }).then((response) => {return response.json()})
+    .then((fruit)=>{
+      this.addNewFruit(fruit)
+    })
+  }
+
+  addNewFruit(fruit){
+    this.setState({
+      fruits: this.state.fruits.concat(fruit)
+    })
   }
 
   componentDidMount() {
@@ -15,6 +38,7 @@ class Body extends React.Component {
   render(){
     return(
       <div>
+        <NewFruit handleFormSubmit={this.handleFormSubmit} />
         <AllFruits fruits={this.state.fruits} />
       </div>
     )
